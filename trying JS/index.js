56ba7cda610497f90e9af5a4dc96b02cdf46bdbe
Bikaka22 <bikalpakhanal357@gmail.com>
2025-03-06 16:45:30 -0500

    Add files via upload

---
+++ trying JS/index.js
@@ -0,0 +1,12 @@
+//console.log("Hello");
+//console.log("I love pizza!");
+
+
+//console.logs allow you to write stuff
+window.alert("Redirecting you to the page, please wait")
+//these are window alerts.
+
+document.getElementById("myH1").textContent = "Sample";
+document.getElementById("myP").textContent = "";
+
+
